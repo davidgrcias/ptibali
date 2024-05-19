1173b749f2f20624ebb9025e89d956a1bc9823ad
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-05-20 02:03:15 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ThingsToDo from "./components/ThingsToDo";
+import About from "./components/About";
+import Game from "./components/Game";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import CategoryDetails from "./components/CategoryDetails"; // Import the new component
+import Information from "./components/Information";
+import FloatButton from "./components/FloatButton";
+import Cursor from "./components/cursor.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <FloatButton />
+        <Cursor />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Things-to-do" element={<ThingsToDo />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Game" element={<Game />} />
+          <Route path="/tour/:category" element={<CategoryDetails />} />{" "}
+          <Route path="/Information" element={<Information />} />
+          {/* More routes can be added here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
